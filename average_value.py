36276fa9e3d14a6508168d50d70d656e0b3614b8
Drenii <dreni107@hotmail.com>
2024-10-28 01:03:49 +0100

    first problem

---
+++ average_value.py
@@ -0,0 +1,11 @@
+#finding average value of N numbers
+
+num=int(input("How many numbers "))
+sum=0
+
+for i in range(num):
+    numbers=float(input("enter any number "))
+    sum+=numbers
+
+avg=sum/num
+print("Average of ", num, " numbers is " , avg)
