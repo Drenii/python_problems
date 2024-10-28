94f213d69acd5cf742ff71a7f8ef28ca28bd8b2a
Drenii <dreni107@hotmail.com>
2024-10-28 14:52:12 +0100

    change

--- extract_extension.py
+++ extract_extension.py
@@ -1,5 +1,5 @@
 filepath=input("enter file path ")
 file_extns=filepath.split(".")
-
+print("Enter file extension ",file_extns, type(file_extns))
 if len(file_extns)>1:
    print("File extension is:", file_extns[-1])
